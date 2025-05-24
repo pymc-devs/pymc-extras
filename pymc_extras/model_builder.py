a42d27f35d9b372465db5f179ae62f5c3ba12d19
butterman0 <harryhorsley9@gmail.com>
2025-05-24 17:20:57 +0200

    add missing call to validate data

--- pymc_extras/model_builder.py
+++ pymc_extras/model_builder.py
@@ -561,6 +561,8 @@ class ModelBuilder:
         >>> prediction_data = pd.DataFrame({'input':x_pred})
         >>> pred_mean = model.predict(prediction_data)
         """
+        
+        X_pred = self._validate_data(X_pred)
 
         posterior_predictive_samples = self.sample_posterior_predictive(
             X_pred, extend_idata, combined=False, predictions=predictions, **kwargs
