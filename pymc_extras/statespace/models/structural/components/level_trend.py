f6564da4caa9bd413673a61cafe7ebf34e9699e9
jessegrabowski <jessegrabowski@gmail.com>
2025-08-03 23:57:36 +0800

    Add shared_state argument to LevelTrendComponent

--- pymc_extras/statespace/models/structural/components/level_trend.py
+++ pymc_extras/statespace/models/structural/components/level_trend.py
@@ -13,13 +13,11 @@ class LevelTrendComponent(Component):
     Parameters
     ----------
     order : int
-
         Number of time derivatives of the trend to include in the model. For example, when order=3, the trend will
         be of the form ``y = a + b * t + c * t ** 2``, where the coefficients ``a, b, c`` come from the initial
         state values.
 
     innovations_order : int or sequence of int, optional
-
         The number of stochastic innovations to include in the model. By default, ``innovations_order = order``
 
     name : str, default "level_trend"
@@ -27,6 +25,11 @@ class LevelTrendComponent(Component):
 
     observed_state_names : list[str] | None, default None
         List of strings for observed state labels. If None, defaults to ["data"].
+
+    share_states: bool, default False
+        Whether latent states are shared across the observed states. If True, there will be only one set of latent
+        states, which are observed by all observed states. If False, each observed state has its own set of
+        latent states. This argument has no effect if `k_endog` is 1.
 
     Notes
     -----
@@ -120,7 +123,10 @@ class LevelTrendComponent(Component):
         innovations_order: int | list[int] | None = None,
         name: str = "level_trend",
         observed_state_names: list[str] | None = None,
+        share_states: bool = False,
     ):
+        self.share_states = share_states
+
         if innovations_order is None:
             innovations_order = order
 
@@ -156,37 +162,50 @@ class LevelTrendComponent(Component):
         super().__init__(
             name,
             k_endog=k_endog,
-            k_states=k_states * k_endog,
-            k_posdef=k_posdef * k_endog,
+            k_states=k_states * k_endog if not share_states else k_states,
+            k_posdef=k_posdef * k_endog if not share_states else k_posdef,
             observed_state_names=observed_state_names,
             measurement_error=False,
             combine_hidden_states=False,
-            obs_state_idxs=np.tile(np.array([1.0] + [0.0] * (k_states - 1)), k_endog),
+            obs_state_idxs=np.tile(
+                np.array([1.0] + [0.0] * (k_states - 1)), k_endog if not share_states else 1
+            ),
         )
 
     def populate_component_properties(self):
         k_endog = self.k_endog
-        k_states = self.k_states // k_endog
-        k_posdef = self.k_posdef // k_endog
+        k_endog_effective = 1 if self.share_states else k_endog
+
+        k_states = self.k_states // k_endog_effective
+        k_posdef = self.k_posdef // k_endog_effective
 
         name_slice = POSITION_DERIVATIVE_NAMES[:k_states]
         self.param_names = [f"initial_{self.name}"]
         base_names = [name for name, mask in zip(name_slice, self._order_mask) if mask]
-        self.state_names = [
-            f"{name}[{obs_name}]" for obs_name in self.observed_state_names for name in base_names
-        ]
+
+        if self.share_states:
+            self.state_names = [f"{name}[{self.name}_shared]" for name in base_names]
+        else:
+            self.state_names = [
+                f"{name}[{obs_name}]"
+                for obs_name in self.observed_state_names
+                for name in base_names
+            ]
+
         self.param_dims = {f"initial_{self.name}": (f"state_{self.name}",)}
         self.coords = {f"state_{self.name}": base_names}
 
         if k_endog > 1:
+            self.coords[f"endog_{self.name}"] = self.observed_state_names
+
+        if k_endog_effective > 1:
             self.param_dims[f"state_{self.name}"] = (
                 f"endog_{self.name}",
                 f"state_{self.name}",
             )
             self.param_dims = {f"initial_{self.name}": (f"endog_{self.name}", f"state_{self.name}")}
-            self.coords[f"endog_{self.name}"] = self.observed_state_names
 
-        shape = (k_endog, k_states) if k_endog > 1 else (k_states,)
+        shape = (k_endog_effective, k_states) if k_endog_effective > 1 else (k_states,)
         self.param_info = {f"initial_{self.name}": {"shape": shape, "constraints": None}}
 
         if self.k_posdef > 0:
@@ -196,20 +215,23 @@ class LevelTrendComponent(Component):
                 name for name, mask in zip(name_slice, self.innovations_order) if mask
             ]
 
-            self.shock_names = [
-                f"{name}[{obs_name}]"
-                for obs_name in self.observed_state_names
-                for name in base_shock_names
-            ]
+            if self.share_states:
+                self.shock_names = [f"{name}[{self.name}_shared]" for name in base_shock_names]
+            else:
+                self.shock_names = [
+                    f"{name}[{obs_name}]"
+                    for obs_name in self.observed_state_names
+                    for name in base_shock_names
+                ]
 
             self.param_dims[f"sigma_{self.name}"] = (
                 (f"shock_{self.name}",)
-                if k_endog == 1
+                if k_endog_effective == 1
                 else (f"endog_{self.name}", f"shock_{self.name}")
             )
             self.coords[f"shock_{self.name}"] = base_shock_names
             self.param_info[f"sigma_{self.name}"] = {
-                "shape": (k_posdef,) if k_endog == 1 else (k_endog, k_posdef),
+                "shape": (k_posdef,) if k_endog_effective == 1 else (k_endog_effective, k_posdef),
                 "constraints": "Positive",
             }
 
@@ -218,12 +240,14 @@ class LevelTrendComponent(Component):
 
     def make_symbolic_graph(self) -> None:
         k_endog = self.k_endog
-        k_states = self.k_states // k_endog
-        k_posdef = self.k_posdef // k_endog
+        k_endog_effective = 1 if self.share_states else k_endog
+
+        k_states = self.k_states // k_endog_effective
+        k_posdef = self.k_posdef // k_endog_effective
 
         initial_trend = self.make_and_register_variable(
             f"initial_{self.name}",
-            shape=(k_states,) if k_endog == 1 else (k_endog, k_states),
+            shape=(k_states,) if k_endog_effective == 1 else (k_endog, k_states),
         )
         self.ssm["initial_state", :] = initial_trend.ravel()
 
@@ -231,27 +255,34 @@ class LevelTrendComponent(Component):
         T = pt.zeros((k_states, k_states))[triu_idx[0], triu_idx[1]].set(1)
 
         self.ssm["transition", :, :] = pt.specify_shape(
-            pt.linalg.block_diag(*[T for _ in range(k_endog)]), (self.k_states, self.k_states)
+            pt.linalg.block_diag(*[T for _ in range(k_endog_effective)]),
+            (self.k_states, self.k_states),
         )
 
         R = np.eye(k_states)
         R = R[:, self.innovations_order]
 
         self.ssm["selection", :, :] = pt.specify_shape(
-            pt.linalg.block_diag(*[R for _ in range(k_endog)]), (self.k_states, self.k_posdef)
+            pt.linalg.block_diag(*[R for _ in range(k_endog_effective)]),
+            (self.k_states, self.k_posdef),
         )
 
         Z = np.array([1.0] + [0.0] * (k_states - 1)).reshape((1, -1))
 
-        self.ssm["design", :, :] = pt.specify_shape(
-            pt.linalg.block_diag(*[Z for _ in range(k_endog)]), (self.k_endog, self.k_states)
-        )
+        if self.share_states:
+            self.ssm["design", :, :] = pt.specify_shape(
+                pt.join(0, *[Z for _ in range(k_endog)]), (self.k_endog, self.k_states)
+            )
+        else:
+            self.ssm["design", :, :] = pt.specify_shape(
+                pt.linalg.block_diag(*[Z for _ in range(k_endog)]), (self.k_endog, self.k_states)
+            )
 
         if k_posdef > 0:
             sigma_trend = self.make_and_register_variable(
                 f"sigma_{self.name}",
-                shape=(k_posdef,) if k_endog == 1 else (k_endog, k_posdef),
+                shape=(k_posdef,) if k_endog_effective == 1 else (k_endog, k_posdef),
             )
-            diag_idx = np.diag_indices(k_posdef * k_endog)
+            diag_idx = np.diag_indices(k_posdef * k_endog_effective)
             idx = np.s_["state_cov", diag_idx[0], diag_idx[1]]
             self.ssm[idx] = (sigma_trend**2).ravel()
