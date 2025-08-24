89c6bc0d49a3c9e52f362d4bc19488cf0a187c6c
Jesse Grabowski <48652735+jessegrabowski@users.noreply.github.com>
2025-08-24 07:31:51 +0200

    Allow exogenous regressions in `BayesianSARIMAX` (#566)
    * Re-arrange docstrings
    
    * Simplify how exog data is stored
    
    * Allow exogenous variables in BayesianSARIMAX, rename SARIMA -> SARIMAX
    
    * Use constant for exogenous dim

--- pymc_extras/statespace/__init__.py
+++ pymc_extras/statespace/__init__.py
@@ -1,12 +1,12 @@
 from pymc_extras.statespace.core.compile import compile_statespace
 from pymc_extras.statespace.models import structural
 from pymc_extras.statespace.models.ETS import BayesianETS
-from pymc_extras.statespace.models.SARIMAX import BayesianSARIMA
+from pymc_extras.statespace.models.SARIMAX import BayesianSARIMAX
 from pymc_extras.statespace.models.VARMAX import BayesianVARMAX
 
 __all__ = [
     "BayesianETS",
-    "BayesianSARIMA",
+    "BayesianSARIMAX",
     "BayesianVARMAX",
     "compile_statespace",
     "structural",
