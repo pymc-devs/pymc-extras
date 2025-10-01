287cb57790c05c3310aadacfab734dd68c9ac21e
Alexandre Andorra <andorra.alexandre@gmail.com>
2025-10-01 11:18:56 -0400

    make test less arbitrary
    Co-authored-by: Jesse Grabowski <48652735+jessegrabowski@users.noreply.github.com>

--- tests/statespace/models/structural/components/test_autoregressive.py
+++ tests/statespace/models/structural/components/test_autoregressive.py
@@ -166,7 +166,7 @@ def test_autoregressive_shared_and_not_shared():
 
     # make sure param_info is correct
     # shound't have endog state when share_states is True
-    assert "endog_" not in shared.param_info["params_shared_ar"]["dims"][0]
+    assert not any(dim.startwith("endog_") for dim in shared.param_info["params_shared_ar"]["dims"])
     assert shared.param_info["sigma_shared_ar"]["dims"] is None
 
     individual = st.AutoregressiveComponent(
