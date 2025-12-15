f19fa5c9eaff9bb6b430d57dc6b38cc3d123d34b
Jiya Gupta <141018273+Jiya873@users.noreply.github.com>
2025-12-15 23:00:53 +0530

    Refactor: rename AutoregressiveComponent to Autoregressive

--- pymc_extras/statespace/models/structural/components/autoregressive.py
+++ pymc_extras/statespace/models/structural/components/autoregressive.py
@@ -11,7 +11,7 @@ from pymc_extras.statespace.models.structural.core import Component
 from pymc_extras.statespace.models.structural.utils import order_to_mask
 
 
-class AutoregressiveComponent(Component):
+class Autoregressive(Component):
     r"""
     Autoregressive timeseries component
 
@@ -64,7 +64,7 @@ class AutoregressiveComponent(Component):
         import pytensor.tensor as pt
 
         trend = st.LevelTrendComponent(order=1, innovations_order=0)
-        ar = st.AutoregressiveComponent(2)
+        ar = st.Autoregressive(2)
         ss_mod = (trend + ar).build()
 
         with pm.Model(coords=ss_mod.coords) as model:
@@ -239,3 +239,6 @@ class AutoregressiveComponent(Component):
 
         cov_idx = ("state_cov", *np.diag_indices(k_posdef))
         self.ssm[cov_idx] = sigma_ar**2
+
+
+AutoregressiveComponent = Autoregressive
